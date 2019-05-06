c9bad159b786fcb1a93b5c3f1ef3d2cf6c5e9793
Rhadá Costa <rhadacosta@gmail.com>
2019-05-06 20:45:10 -0300

    reload app

--- src/app/books/book-detail/reviews-book/reviews.model.ts
+++
@@ -1,7 +0,0 @@
-export interface ReviewofBook {
-  name: string;
-  date: string;
-  rating: number;
-  comments: string;
-  livroId: string;
-}
--- src/app/carrousel/carrousel.model.ts
+++
@@ -1,7 +0,0 @@
-export interface SlideCarrousel {
-  id: string;
-  title: string;
-  description: number;
-  link: string;
-  image: string;
-}
